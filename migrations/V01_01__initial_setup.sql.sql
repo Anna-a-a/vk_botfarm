dbc1a655a31327d6fd84cd6e0f0f44a51bea3f16
Anna-a-a <alikinaan8@yandex.ru>
2025-11-23 23:10:59 +0500

    init db

---
+++ migrations/V01_01__initial_setup.sql.sql
@@ -0,0 +1,19 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    created_ad TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    login VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    project_id UUID NOT NULL,
+    env VARCHAR(50) NOT NULL,
+    domain VARCHAR(50) NOT NULL,
+    locktime TIMESTAMP WITH TIME ZONE
+);
+
+COMMENT ON TABLE users IS 'Пользователи';
+COMMENT ON COLUMN users.created_ad IS 'Дата создания пользователя';
+COMMENT ON COLUMN users.login IS 'Почтовый адрес пользователя';
+COMMENT ON COLUMN users.password IS 'Пароль пользователя';
+COMMENT ON COLUMN users.project_id IS 'UUID проекта, к которому принадлежит пользователь';
+COMMENT ON COLUMN users.env IS 'Название окружения';
+COMMENT ON COLUMN users.domain IS 'Тип пользователя';
+COMMENT ON COLUMN users.locktime IS 'Временная метка';
